70ea6ebfc6861f05f835c9b242595201a3fc8313
Timur <lollipop.fly@gmail.com>
2023-09-18 12:25:45 +0300

    Change  favicon & add navigation

--- src/widgets/header/ui/Header.vue
+++ src/widgets/header/ui/Header.vue
@@ -1,5 +1,9 @@
 <template>
-  <v-app-bar title="Polka">
+  <v-app-bar>
+    <template v-slot:prepend>
+      <Logo />
+    </template>
+
     <template v-slot:append>
       <ThemeSwitcher />
     </template>
@@ -8,4 +12,5 @@
 
 <script setup lang="ts">
 import { ThemeSwitcher } from '@/features/theme-switcher/'
+import { Logo } from '@/shared/ui/logo/'
 </script>
