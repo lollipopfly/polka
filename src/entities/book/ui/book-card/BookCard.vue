39591423c85b274b9d0732cac954b4e40e7e0814
Timur <lollipop.fly@gmail.com>
2023-11-20 17:23:15 +0300

    feature-17/Favorites page

--- src/entities/book/ui/book-card/BookCard.vue
+++ src/entities/book/ui/book-card/BookCard.vue
@@ -1,18 +1,31 @@
 <template>
-  <v-card>
-    <v-img src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg" height="200px" cover></v-img>
+  <v-card link :href="href" :class="css.card">
+    <div :class="css['card__top']">
+      <v-icon icon="mdi-star" :class="css['card__icon']" color="yellow-darken-2" />
+      <v-img v-if="image" :src="image" :class="css['card__img']" cover />
+      <v-img
+        v-else
+        :src="'/src/shared/ui/assets/no-image.png'"
+        :class="css['card__img']"
+        alt="No image"
+        cover
+      />
+    </div>
 
-    <v-card-title>Война и Мир</v-card-title>
-
-    <v-card-subtitle>"Лев Толстой</v-card-subtitle>
-
-    <v-card-text
-      >Творчество Льва Николаевича Толстого — уникальное явление в истории мировой культуры.
-      Писатель, философ и гуманист создал ...</v-card-text
-    >
-
-    <v-card-actions>
-      <v-btn variant="outlined">Подробнее</v-btn>
-    </v-card-actions>
+    <v-card-title>{{ data.title }}</v-card-title>
+    <v-card-subtitle>{{ getAuthors(data.authors) }}</v-card-subtitle>
   </v-card>
 </template>
+
+<script setup lang="ts">
+import { type IBook, getAuthors } from '@/entities/book'
+import css from './css.module.css'
+
+const props = defineProps<{
+  data: IBook
+}>()
+
+const image = computed((): string | undefined => props.data?.image)
+
+const href = computed((): string => `/book/${props.data.id}`)
+</script>
