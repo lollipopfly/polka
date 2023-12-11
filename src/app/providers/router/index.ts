b130024bdd1f619c0ca2dff573beefba574c4971
Timur <lollipop.fly@gmail.com>
2023-12-11 17:33:33 +0300

    feature-29/move routes from pages to provider

--- src/app/providers/router/index.ts
+++ src/app/providers/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import { routes } from '@/pages'
+import { routes } from './routes'
 
 export const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
--- src/app/providers/router/routes.ts
+++ src/app/providers/router/routes.ts
@@ -1,35 +1,32 @@
-import Routing from './index.vue'
 import { HomeLayout } from '@/widgets/home-layout/'
 
 export const routes = [
   {
     path: '/',
     name: 'home',
-    component: () => import('./home'),
+    component: () => import('@/pages/home'),
     meta: {
       layout: HomeLayout
     }
   },
   {
     path: '/favorites',
-    component: () => import('./favorites')
+    component: () => import('@/pages/favorites')
   },
   {
     path: '/bookshelf',
-    component: () => import('./bookshelf')
+    component: () => import('@/pages/bookshelf')
   },
   {
     path: '/book/:id',
     name: 'Book',
-    component: () => import('./book')
+    component: () => import('@/pages/book')
   },
   {
     path: '/:pathMatch(.*)',
-    component: () => import('./not-found'),
+    component: () => import('@/pages/not-found'),
     meta: {
       layout: HomeLayout
     }
   }
 ]
-
-export { Routing }
